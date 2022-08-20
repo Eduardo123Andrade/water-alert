9bd8095a69734161cfdf6656511d9a8f8988bb5b
Eduardo Andrade <eduardo.andrade15@hotmail.com>
2022-08-20 18:23:07 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import { Home } from './screens';
+
+function App() {
+  return (
+    <div className="App">
+      <body className='Body'>
+        <Home />
+      </body>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/screens/Home.tsx
@@ -0,0 +1,12 @@
+
+
+import React from 'react'
+
+
+export const Home = () => {
+  return (
+    <div>
+      <label>Hello world</label>
+    </div>
+  )
+}
