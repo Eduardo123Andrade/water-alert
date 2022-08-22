99331f4bbd98329519ee03916a18361190830000
Eduardo Andrade <eduardo.andrade15@hotmail.com>
2022-08-21 21:23:35 -0300

    created intial modal layout

--- src/modals/Modal.tsx
+++ src/modals/Modal.tsx
@@ -1,15 +1,49 @@
+import { useState } from 'react'
+import { Button, InputTimer } from '../components'
 import './Modal.css'
 
 
 export const Modal = () => {
+  const [initialHour, setInitialHour] = useState<string>('')
+  const [finalHour, setFinalHour] = useState<string>('')
+  const [intervalTime, setIntervalTime] = useState<string>('')
+
+
   return (
     <div className='Modal-background'>
       <div id='Card'>
-        <label>Hora inicial</label>
 
-        <label>Hora final</label>
+        <div id='Close-button'>
+          <label id='Close-label' onClick={() => console.log('close')}>
+            X
+          </label>
+        </div>
 
-        <label>Tempo de intervalo</label>
+        <div id='Children'>
+          <InputTimer
+            title='Hora inicial:'
+            value={initialHour}
+            onChange={setInitialHour}
+          />
+
+          <InputTimer
+            title='Hora final:'
+            value={finalHour}
+            onChange={setFinalHour}
+          />
+
+          <InputTimer
+            title='Tempo de intervalo:'
+            value={intervalTime}
+            onChange={setIntervalTime}
+          />
+        </div>
+        <div id='Footer'>
+          <Button
+            title='Salvar'
+            onPress={() => console.log('pressed')}
+          />
+        </div>
       </div>
     </div>
   )
