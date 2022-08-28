efff484eab2b28823aee0a166232498d3ef7dd16
Eduardo Andrade <eduardo.andrade15@hotmail.com>
2022-08-28 00:20:38 -0300

    created water modal

---
+++ src/modals/water-modal/WaterModal.tsx
@@ -0,0 +1,28 @@
+import { Button } from "../../components"
+import { BaseModal } from "./../index"
+
+import "./WaterModal.css"
+
+
+type WaterModalProps = {
+  visible: boolean
+  onRequestClose: () => void
+}
+
+
+export const WaterModal = (props: WaterModalProps) => {
+  return (
+    <BaseModal {...props} visible>
+      <div id='Water-Modal-Body'>
+        <label id='Water-Modal-label'>
+          Beba agua
+        </label>
+
+        <Button
+          title='Bebi'
+          onPress={props.onRequestClose}
+        />
+      </div>
+    </BaseModal>
+  )
+}
---
+++ src/modals/water-modal/index.ts
@@ -0,0 +1,1 @@
+export { WaterModal } from './WaterModal'
